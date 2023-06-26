da5ccebd14f773b7cb48dc15f0370f0c14f854ae
J-Sabejon24 <sabejon9000@gmail.com>
2023-06-27 00:56:05 +0800

    New Update

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// SELECTS MOBILE NAVIGATION
+const mobileNav = document.querySelector(".mobile__nav");
+const mobileNavBtn = document.querySelector(".fa-bars");
+const clostMobileNav = document.querySelector(".fa-x");
+
+
+// TOGGLE BUTTONS FOR THE MOBILE NAVBAR
+mobileNavBtn.addEventListener("click", () => {
+	mobileNav.classList.add("open");
+});
+
+clostMobileNav.addEventListener("click", () => {
+	mobileNav.classList.remove("open");
+});
+
+// FOR CHECKOUT 
+
+
+
